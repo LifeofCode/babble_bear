50e223eb2da58f04a1ee55d9f76eafb04edba6cb
LifeofCode <LifeofCode@users.noreply.github.com>
2015-08-18 01:06:08 +0000

    categories page outline done, start of topics page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 @c1 = Category.create(name: 'Vocabulary', example_pic: 'https://twinkl.co.uk/image/resource_preview_xlarge/T-T-6251-French-Fruit-and-Vegtable-Vocabulary-Cards.jpg')
 @c2 = Category.create(name: 'Verb Introduction', example_pic: 'http://www.carlexonline.com/images/21109b.jpg')
 @c3 = Category.create(name: 'Simple Phrases', example_pic: 'http://1.bp.blogspot.com/-E4-38gvAWPc/UyS3VhWgvzI/AAAAAAAABTo/-dtlOeHYmhw/s1600/freeblog5.png')
-@c4 = Category.create(name: 'Present Tense', example_pic: 'http://www.quia.com/files/quia/users/frenchwarrior/verb-signs')
+@c4 = Category.create(name: 'Asking Questions', example_pic: 'http://www.quia.com/files/quia/users/frenchwarrior/verb-signs')
+@c5 = Category.create(name: 'Reading', example_pic: 'http://www.quia.com/files/quia/users/frenchwarrior/verb-signs')
+@c6 = Category.create(name: 'ER, IR, RE Verbs', example_pic: 'http://www.quia.com/files/quia/users/frenchwarrior/verb-signs')
 
 Topic.create(name: "Animals", category_id: @c1.id)
 Topic.create(name: "Fruits", category_id: @c1.id)
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-  // $(".category-name").on("click", function(){
-  //   $.ajax({
-  //     url: "/topics",
-  //     method: "GET",
-  //     dataType: "json"
-  //   }).done(function(topics){
-  //     console.log(topics)
-  //     $("body").empty();
-  //     $(topics).each(function(index, topic){
-  //       $("body").append(topic.name);
-  //     })
-  //   });
-  // });
-
   $(".category").on("click", function(){
-    $(this).addClass("move-left");
+    $.ajax({
+      url: "/topics",
+      method: "GET",
+      dataType: "json"
+    }).done(function(topics){
+      console.log(topics)
+      $(".categories").empty();
+      $(topics).each(function(index, topic){
+        $(".categories").append(topic.name);
+      })
+    });
   });
+  
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
